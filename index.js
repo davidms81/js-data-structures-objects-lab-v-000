a2e8be2200164a7494ebfa9aea9c66f9b2b57fc7
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-18 18:01:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   driver[key] = value
   return driver
 }
+
+function deleteFromDriverByKey(driver, key){
+  const newDriver = {...driver}
+  delete newDriver.key
+  return newDriver
+}
